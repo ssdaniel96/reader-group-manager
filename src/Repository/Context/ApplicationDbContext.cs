46cfd338692bed1a60f5b4e118d95365d9ec7a8b
ssdaniel96 <daniel_soares2007@hotmail.com>
2024-07-18 14:19:55 -0300

    apply configuration in context

--- src/Repository/Context/ApplicationDbContext.cs
+++ src/Repository/Context/ApplicationDbContext.cs
@@ -5,6 +5,16 @@ namespace Repository.Context;
 
 public class ApplicationDbContext : DbContext
 {
-    public DbSet<Group> Groups { get; set; }
-    
+    public DbSet<Group> Groups { get; init; }
+    public DbSet<Book> Books { get; init; }
+    public DbSet<Member> Members { get; init; }
+    public DbSet<ReadingPlan> ReadingPlans { get; init; }
+    public DbSet<Reminder> Reminders { get; init; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+    }
 }
